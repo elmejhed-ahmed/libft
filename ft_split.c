ade26434f49bff2f3cc3ef1251b42e4c7727aa4a
elmejhed-ahmed <ahmedelmajhed11@gmail.com>
2023-11-11 21:38:28 +0100

    libft

--- ft_split.c
+++ ft_split.c
@@ -6,70 +6,86 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:27:17 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/10 17:51:09 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/11 21:35:57 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-static int count_w(char coust *str, char ca)
+#include "libft.h" 
+
+static int count_w(char *str, char ca)
 {
-    int pos;
-    int i= 0;
-    int count;
-    count = 0;
-    pos = 0;
-    while (str[i])
-    {
-        if(pos == 0 && str[i] != c)
+    
+   int pos;
+   int count;
+   int i = 0;
+   pos = 1;
+   count = 0;
+   while (*str)
+   {
+        if (*str != ca && pos == 1)
         {
-            pos = 1;
-            count ++;
-            i++;
+            count++;
+            while (str[i] != ca)
+            {
+                i++;
+            }
+            pos = 0;
+            
         }
-        else if (str[i] == c))
+        else if (*str == ca)
+            pos = 1;
+        str++;
+   }
+   return count;
+}
+static int count_size_wo(char *st,char c)
+{
+   int pos;
+   int i = 0;
+   pos = 1;
+   while (*str)
+   {
+        if (*str != ca && pos == 1)
         {
+            while (str[i] != ca)
+            {
+                i++;
+            }
             pos = 0;
         }
-        i++;
-    }
+        else if (*str == ca)
+            pos = 1;
+        str++;
+   }
+   return i;
 }
 char **ft_split(char const *s, char c)
 {
+    int cw;
     int i;
-    le
-    while (s[i] !='\0')
+    char **ml;
+
+    wc = count_w(s,c);
+    i = 0;
+    //
+    ml = (char **)malloc(sizeof(char *) * wc + 1);
+    // if string is null
+    if (!s)
+        return NULL;
+    //
+    if(!ml)
+        return 0;
+    int count_size = count_size_wo(*s,);
+    while (*s)
     {
-        
+        ml[wc][i] = 
     }
     
 }
-/*
-#include <stdio.h>
-int count_w(char  *str, char ca)
+
+int main()
 {
-    int pos;
-    int i= 0;
-    int count;
-    count = 0;
-    pos = 0;
-    while (str[i])
-    {
-        if(pos == 0 && str[i] != ca)
-        {
-            pos = 1;
-            count ++;
-        }
-        else if (str[i] == ca)
-        {
-            pos = 0;
-        }
-        i++;
-    }
+    char *cc = "       eerwt  rtr   lkfjkh jhfajksh khadjsfhh kjfhajks jkhfkadjs   sfrqrt";
+    char c = ' ';
+    printf("%d", count_w(cc,c));
 }
-int main() 
-{
-  char *c= "////////hel///low/sa/ll/////kk///////////";
-  char d = '/';
-  printf("%d",count_w(c,d));
-}
-*/
