0dbbbe0db5ad7ff4cb4795bc1114f533b1b9fc08
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-17 10:11:16 +0100

    libft

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,15 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 17:02:38 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/16 18:52:40 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/17 10:08:37 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-t_list *ft_lstnew(void *content)
-{
-    t_list *node;
-    node = (t_list *)malloc(sizeof(t_list));
-    
-}
