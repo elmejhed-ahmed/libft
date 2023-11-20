7e9b3e8d715c0d5ccc43b767a5ebae19f78ab48a
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-20 10:18:16 +0100

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 14:38:05 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/20 10:15:47 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/20 10:18:12 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,4 +63,5 @@ t_list  *ft_lstnew(void *content);
 void    ft_lstadd_front(t_list **lst, t_list *new);
 int     ft_lstsize(t_list *lst);
 t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new);
 #endif
