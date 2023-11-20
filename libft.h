e3686b79266e964997017addbbc159fabf3e2faf
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-20 09:42:08 +0100

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 14:38:05 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/19 20:15:48 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/20 09:31:37 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,4 +61,5 @@ typedef struct s_list
 t_list  *ft_lstnew(void *content);
 void    ft_lstadd_front(t_list **lst, t_list *new);
 int     ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
 #endif
