8d3365dad862b523fe36bfce1aae5660c034a485
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-21 13:59:57 +0100

    libft

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:51:56 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/21 10:53:22 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/21 13:59:49 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void ft_lstclear(t_list **lst, void (*del)(void*))
 {
+    t_list *node;
+    t_list *tp;
     
+    node = *lst;
+    while (node != NULL)
+    {
+        tp = node->next;
+        del(node->next);
+        free(node);
+        tp = node;
+    }
 }
