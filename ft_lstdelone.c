d237dca6a211b8c49a725839ed465ff92fa6822a
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-21 10:46:12 +0100

    libft

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 13:13:36 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/20 13:14:40 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/21 10:45:34 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 void ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-    
+    del(lst->content);
+    free(lst);
 }
+//int main()
+// {
+//     t_list *head;
+//     t_list *n1 =ft_lstnew("hellow");
+//     t_list *n2 =ft_lstnew("del");
+//     t_list *n3 =ft_lstnew("lk");
+
+//     head = n1;
+//     n1->next = n2;
+//     n2->next = n3;
+//     ft_lstdelone(n3);
+//     while(head != NULL)
+//     {
+//         printf("%s",head->content);
+//         head = head->next;
+//     }
+// }
