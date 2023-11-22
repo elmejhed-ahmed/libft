a90292a194925df58f26cc971bb2b0f0fa5a7bbb
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-22 11:14:46 +0100

    libft

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:42:12 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/21 17:11:57 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/21 17:36:59 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,13 @@ t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
         nw = ft_lstnew(f(lst->content));
         if(!nw)
         {
-            
-            
+            ft_lstclear(&head,del);
+            return (0);
         }
+        ft_lstadd_back(&head,new);
+        lst = lst->next;
     }
+    return head;
 }
 int main()
 {
