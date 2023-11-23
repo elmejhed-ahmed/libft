7a9b2bca4d4a1f6f6b5cf17286b2d2a5621cd3f4
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-23 11:55:20 +0100

    libft

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,16 +6,12 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:51:56 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/23 11:37:36 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/23 11:55:04 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void del(void *con)
-{
-    if(con != NULL)
-        free(con);
-}
+
 void ft_lstclear(t_list **lst, void (*del)(void*))
 {
     t_list *node;
@@ -32,18 +28,4 @@ void ft_lstclear(t_list **lst, void (*del)(void*))
         node = tp;
     }
     *lst = NULL;
-}
-int main ()
-{
-    t_list *node;
-    t_list *n1 = ft_lstnew(ft_strdup("hel"));  // Use strdup to create a copy of the string
-    t_list *n2 = ft_lstnew(ft_strdup("hur"));
-
-
-    node = n1;
-    n1->next = n2;
-    n2->next = NULL;
-    ft_lstclear(&node, &del);
-
-    return 0;
 }
