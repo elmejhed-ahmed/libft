a80ca28ebf043b5f6c0ee3e2e5c01f72f777eca0
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-25 19:02:30 +0100

    libft

--- ft_atoi11.c
+++ ft_atoi11.c
@@ -6,34 +6,41 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 13:42:04 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/25 18:46:28 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/25 19:00:51 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
-static int	chack_nbr(unsigned long long nbr, int si, int leni)
+static int chack_nbr(unsigned long long nbr, int si, int leni, int count)
 {
-	if (leni >= 20 && si > 0)
-		return (-1);
-	if (leni >= 20 && si < 0)
-		return (0);
-	if (nbr > LONG_MAX && si > 0)
-		return (-1);
-	if (nbr > LONG_MAX && si < 0)
-		return (0);
-	return ((int)nbr * si);
+	if(leni == count)
+	{
+		if (leni >= 20 && si > 0)
+			return (-1);
+		if (leni >= 20 && si < 0)
+			return (0);
+		if (nbr > LONG_MAX && si > 0)
+			return (-1);
+		if (nbr > LONG_MAX && si < 0)
+			return (0);
+		return ((int)nbr * si);
+	}
+	else 
+		return(nbr * si);
 }
-
 int	ft_atoi(const char *str)
 {
-	int					i;
-	int					negat;
+	int	i;
+	int	negat;
+	int count;
 	unsigned long long	num;
 
 	i = 0;
 	negat = 1;
 	num = 0;
+	count = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == '-' || str[i] == '+')
@@ -42,10 +49,12 @@ int	ft_atoi(const char *str)
 			negat = -1;
 		i++;
 	}
+	while ( str[i] != '\0' && str[i] == 48)
+    	i++;
 	while (str[i] >= 48 & str[i] <= 57)
 	{
-		num = num * 10 + str[i] - 48;
-		i++;
+		num = num * 10 + str[i++] - 48;
+		count++;
 	}
-	return (chack_nbr(num, negat, i));
+	return (chack_nbr(num, negat, i , count));
 }
