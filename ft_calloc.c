6ddec8b1210be4bf94ba74df8d35a1be563a5c13
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2023-11-30 16:39:51 +0100

    Auto commit at 2023-11-30 16:39:51

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:20:12 by ael-mejh          #+#    #+#             */
-/*   Updated: 2023/11/30 16:23:48 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2023/11/30 16:35:16 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*d;
-	
+
 	if (count < 0 || size < 0)
-		return 0;
+		return (0);
 	d = (char *)malloc(count * size);
 	if (!d)
 		return (0);
 	else if (count == 0 || size == 0)
-		ft_bzero(d,1);
+		ft_bzero(d, 1);
 	else
 		ft_bzero(d, count * size);
 	return (d);
